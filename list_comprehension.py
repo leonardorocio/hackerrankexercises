f1dc1fd3e4e295b28e78807877c1552fe58ce313
leonardorocio <50716715+leonardorocio@users.noreply.github.com>
2020-05-20 10:15:48 -0300

    List comprehension.py

---
+++ list_comprehension.py
@@ -0,0 +1,7 @@
+if __name__ == '__main__':
+    X = int(input())
+    Y = int(input())
+    Z = int(input())
+    N = int(input())
+
+    print([[x, y, z] for x in range(X + 1) for y in range(Y + 1) for z in range(Z + 1)      if x + y + z != N])
